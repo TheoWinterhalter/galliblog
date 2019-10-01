73447cbd9fac5afc931324b730ecc49f3885e179
Théo Winterhalter <isheeft@gmail.com>
2019-10-01 15:54:22 +0200

    Remove unnecessary error management in main

--- src/galliblog.ml
+++ src/galliblog.ml
@@ -33,20 +33,6 @@ let month = function
 let date_text (d,m,y) =
   Printf.sprintf "On %d %s %d." d (month m) y
 
-exception Error of string
-
-let error fmt = Printf.ksprintf (fun s -> raise (Error s)) fmt
-
-let print_position lexbuf =
-  let open Lexing in
-  let pos = lexbuf.lex_curr_p in
-  let str = Lexing.lexeme lexbuf in
-  let begchar = pos.pos_cnum - pos.pos_bol + 1 in
-  Printf.sprintf "In %s, line %d, characters %d-%d : %s"
-    pos.pos_fname pos.pos_lnum begchar
-    (begchar + (String.length str))
-    (Lexing.lexeme lexbuf)
-
 let () =
   fmkdir "website" ;
   copy "content/blog.css" "website/blog.css" ;
